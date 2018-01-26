eff54a68eda1c041fcaefbf570b04f3fabd0dea2
Manodhayan <33039242+Manodhayan@users.noreply.github.com>
2018-01-27 00:42:31 +0530

    Add files via upload

---
+++ Exploratory Data Analysis/plot1.R
@@ -0,0 +1,11 @@
+#Load power consumption data
+powcon <- read.table("household_power_consumption.txt",skip=1,sep=";")
+names(powercon) <- c("Date","Time","Global_active_power","Global_reactive_power","Voltage","Global_intensity","Sub_metering_1","Sub_metering_2","Sub_metering_3")
+twodays <- subset(powercon,powercon$Date=="1/2/2007" | powercon$Date =="2/2/2007")
+
+#Basic plot function
+hist(as.numeric(as.character(twodays$Global_active_power)),col="red",main="Global Active Power",xlab="Global Active Power(kilowatts)")
+
+# Graph Annotation
+title(main="Global Active Power")
+
